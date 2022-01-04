c66a7d3f5cc3a559d9316ba17504fa6cea66920d
Glitch (glitch-hello-website) <none>
2022-01-04 23:05:56 +0700

    firt commit

---
+++ script7_2.js
@@ -0,0 +1,23 @@
+//output: console.log/alert
+//input: prompt("nhap vao")
+let toan = prompt("Nhap Vao Diem Toan")
+let ly = prompt("Nhap Vao Diem Ly")
+let hoa = prompt("Nhap Vao Diem Hoa")
+
+let ave = (Number(toan) + Number(ly) +Number(hoa))/3;
+console.log(ave.toFixed(1))
+
+let _number = "9";
+//_number++ = 10;
+//_number = _number + 1; //<=> _number += 1; _number ++ 1
+console.log(_number++)
+//tien to: 
+//++_number
+
+console.log(_number++ + ++_number + _number++);
+/*_number++ = 9;
+++_number = 11;*/
+
+//b1: Tao ra 1 ban _number copy=9 (_number++)
+//b2: a = _number copy +1
+//b3: tra ve a (++_number: a + 1)
---
+++ script7_3.js
@@ -0,0 +1,35 @@
+let name="Phong"
+alert(`hello ${name} ${name}`)
+
+let _name ="Luong Hong Phong";
+let rep = `baby Em nhu bong hoa
+Nhung nguoi hai dau phai ta
+Em voi mang bao cau ca
+Troi ve noi xa xa xa`
+//length
+console.log(_name.length)
+//replace
+console.log(_name.replace("Son","Phong"));
+console.log(rep.replace(/Em/g,"Trang"));
+//find
+console.log(rep.search("Em"));
+//cut string
+//substr
+console.log(_name.substr(10,3));
+//substring
+console.log(_name.substring(0,3))
+//gop chuoi
+
+let _new =" Lam";
+const _newname1 = _name + " Lam";
+const _newname2 = _name + _new;
+console.log(_newname1);
+console.log(_newname2);
+console.log(_name.replace(/n/g,"l"))
+console.log(_name.substr(6,4))
+console.log(_name.substring(6,10))
+console.log(_name.replace(/\s+/g,''))
+//regex js
+let _number = 9;
+console.log(_number++);
+console.log(_number++ + ++_number + _number++);
